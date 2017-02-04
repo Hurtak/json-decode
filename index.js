75d63756c628af2977332a2dc5c76ae42f5d9c4a
Petr Huřťák <petr.hurtak@gmail.com>
2017-02-04 22:23:47 +0100

    formatting

--- index.js
+++ index.js
@@ -2,13 +2,13 @@
 
 const _ = require('lodash')
 
-const type = {
-  null: 0,
-  bool: 1,
-  number: 2,
-  string: 3,
-  array: 4,
-  object: 5
+const Type = {
+  NULL: 0,
+  BOOL: 1,
+  NUMBER: 2,
+  STRING: 3,
+  ARRAY: 4,
+  OBJECT: 5
 }
 
 function jsonDecode (data, decoder) {
@@ -24,10 +24,10 @@ function jsonDecode (data, decoder) {
 
 function decoderToType (input) {
   switch (input) {
-    case null: return type.null
-    case Boolean: return type.bool
-    case Number: return type.number
-    case String: return type.string
+    case null: return Type.NULL
+    case Boolean: return Type.BOOL
+    case Number: return Type.NUMBER
+    case String: return Type.STRING
     default:
       throw new Error('TODO array')
   }
@@ -37,22 +37,22 @@ function dataToAst (input) {
   const inputType = dataToType(input)
 
   switch (inputType) {
-    case type.null:
-    case type.bool:
-    case type.number:
-    case type.string:
+    case Type.NULL:
+    case Type.BOOL:
+    case Type.NUMBER:
+    case Type.STRING:
       return {
         value: input,
         type: inputType,
         children: null
       }
-    case type.array:
+    case Type.ARRAY:
       return {
         value: input,
         type: inputType,
         children: input.map(dataToAst)
       }
-    case type.object:
+    case Type.OBJECT:
       return {
         value: input,
         type: inputType,
@@ -65,24 +65,24 @@ function dataToAst (input) {
 }
 
 function dataToType (input) {
-  if (_.isNull(input)) return type.null
-  else if (_.isBoolean(input)) return type.bool
-  else if (_.isNumber(input)) return type.number
-  else if (_.isString(input)) return type.string
-  else if (_.isArray(input)) return type.array
-  else if (_.isObject(input)) return type.object
+  if (_.isNull(input)) return Type.NULL
+  else if (_.isBoolean(input)) return Type.BOOL
+  else if (_.isNumber(input)) return Type.NUMBER
+  else if (_.isString(input)) return Type.STRING
+  else if (_.isArray(input)) return Type.ARRAY
+  else if (_.isObject(input)) return Type.OBJECT
 
   throw new Error('TODO')
 }
 
 function typeToString (type) {
   switch (type) {
-    case type.null: return 'null'
-    case type.bool: return 'bool'
-    case type.number: return 'number'
-    case type.string: return 'string'
-    case type.array: return 'array'
-    case type.object: return 'object'
+    case Type.NULL: return 'null'
+    case Type.BOOL: return 'bool'
+    case Type.NUMBER: return 'number'
+    case Type.STRING: return 'string'
+    case Type.ARRAY: return 'array'
+    case Type.OBJECT: return 'object'
     default: return 'unknown'
   }
 }
